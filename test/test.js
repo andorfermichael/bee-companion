138e179b9af2d2d9bf380a7791144d0902c29cf2
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2017-01-23 18:33:04 +0100

    [base] update test application

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var should = require("should");
 
 // This agent refers to PORT where the program is running.
 
-var server = supertest.agent("https://localhost:4000");
+var server = supertest.agent("/");
 
 // UNIT test begin
 
