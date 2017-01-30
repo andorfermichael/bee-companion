f252cc5faca2d189aea9d93bf61ad8c1586440ee
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2017-01-30 18:56:19 +0100

    [chore] add test api

---
+++ server/server.js
@@ -0,0 +1,34 @@
+// Express
+var express = require('express');
+var history = require('connect-history-api-fallback');
+var morgan  = require('morgan');
+var bodyParser = require('body-parser');
+
+// Express App
+var app = express();
+
+// Env
+var PORT     = process.env.PORT || 3001;
+var NODE_ENV = process.env.NODE_ENV || 'development';
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Angular Http content type for POST etc defaults to text/plain at
+app.use(bodyParser.text(), function ngHttpFix(req, res, next) {
+  try {
+    req.body = JSON.parse(req.body);
+    next();
+  } catch(e) {
+    next();
+  }
+});
+
+// your api middleware
+var api = require('./test_api')();
+app.use('/api', api);
+
+app.listen(PORT, function() {
+  console.log('Listen on http://localhost:' + PORT + ' in ' + NODE_ENV);
+});
