6fcf2f7ece8cc2d162bff0e36c8787c78e6b228c
Nico Deufemia <ndeufemia.mmt-b2014@fh-salzburg.ac.at>
2017-03-08 23:28:05 +0100

    [feature] add iconfont, improve header, make use of icons, set temporary mainlogo, improve responsibility

--- src/app/+header/header.component.ts
+++ src/app/+header/header.component.ts
@@ -13,7 +13,7 @@ console.log('`Header` component loaded asynchronously');
 @Component({
   selector: 'header',
    // Our list of styles in our component. We may add more to compose many styles together
-  styleUrls: [ './header.component.css' ],
+  styleUrls: [ './header.component.scss' ],
   // Every Angular template is first compiled by the browser before Angular runs it's compiler
   templateUrl: './header.component.html',
 })
