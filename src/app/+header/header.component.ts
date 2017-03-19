6e9efaebf8e095c3c36b1ea040939bf09cbfa750
Nico Deufemia <ndeufemia.mmt-b2014@fh-salzburg.ac.at>
2017-03-19 22:28:55 +0100

    [feature] implement navigator geolocation on header

--- src/app/+header/header.component.ts
+++ src/app/+header/header.component.ts
@@ -19,6 +19,45 @@ console.log('`Header` component loaded asynchronously');
 })
 export class HeaderComponent implements OnInit {
 
+  lat: number = 56.713;
+  lng: number = 21.1644;
+  message: string;
+  messageType: string;
+
+  // public showAlert(msg: string, type: string) {
+  //   this.messageType = type;
+  //   this.message = msg;
+  // }
+
+  public processLocation(data: any) {
+    console.log(data);
+    if (data) {
+      if (data.coords) {
+        this.lat = data.coords.latitude ? data.coords.latitude : this.lat;
+        this.lng = data.coords.longitude ? data.coords.longitude : this.lng;
+        this.messageType = 'success';
+        this.message = 'You successfully granted us retrieving your location to enhance your experience with BeeCompanion.', 'success');
+      }
+    }
+  }  
+
+  public locationError() {
+    this.message = 'Unfortunately we could not aquire your location which is recommended for best user experience with our service.';
+    this.messageType = 'danger';
+  }  
+
+  public enableNavigatorLocation() {
+    console.log("NaviatiorLocation");
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.processLocation.bind(this), this.locationError.bind(this));
+    };
+    // navigator.geolocation.getCurrentPosition(this.processLocation,this.locationError,{
+    //   timeout: 6000,
+    //   enableHighAccuracy: true,
+    //   maximumAge: 0
+    // });
+  }
+
   public ngOnInit() {
     console.log('hello `Header` component');
   }
