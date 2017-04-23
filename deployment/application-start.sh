eb59e5326c4a310f447e0f89fafe6b2461af7bdf
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2017-04-23 12:47:16 +0200

    [base] update build and deployment configuration

--- deployment/application-start.sh
+++ deployment/application-start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-cd var/www/
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+source ~/.bashrc
+nvm install 7.4.0
+nvm use 7.4.0
+npm install -g pm2
+cd /var/www/
+npm install
 cd ./server
 pm2 start server.js
 cd ..
