032cab41099e75e7fa5883f6cd8b02e6ccb5b3b2
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2017-04-23 13:13:01 +0200

    [base] update build and deployment configuration

--- deployment/application-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
-source ~/.bashrc
-nvm install 7.4.0
-nvm use 7.4.0
-npm install -g pm2
-cd var/www/
-npm install
