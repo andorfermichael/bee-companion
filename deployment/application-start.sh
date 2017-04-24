846e03f67e8d35559c167021f8cb671f2efc4e11
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2017-04-24 14:24:36 +0200

    [base] update build and deployment configuration

--- deployment/application-start.sh
+++ deployment/application-start.sh
@@ -3,9 +3,12 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | b
 source ~/.bashrc
 nvm install 7.4.0
 nvm use 7.4.0
+export NODE_ENV=production
 npm install -g pm2
+pm2 kill
 cd /var/www/
 npm install
+npm install -g http-server
 cd ./server
 pm2 start server.js
 cd ..
--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ var beekeepers = require('./routes/beekeepers');
 var app = express();
 
 // Env
-var PORT     = process.env.PORT || 3001;
+var PORT     = process.env.PORT || 3000;
 var NODE_ENV = process.env.NODE_ENV || 'development';
 
 app.use(morgan('dev'));
