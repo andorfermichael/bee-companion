4b0a686b9762686f3ed98a519c596f89fce44254
Nico Deufemia <ndeufemia.mmt-b2014@fh-salzburg.ac.at>
2017-05-02 02:24:43 +0200

    [fix] fix callback url

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -21,7 +21,7 @@ const LoaderOptionsPlugin = require('webpack/lib/LoaderOptionsPlugin');
 const ENV = process.env.ENV = process.env.NODE_ENV = 'development';
 const HOST = process.env.HOST || 'localhost';
 const PORT = process.env.PORT || 8000;
-const AUTH_CB = process.env.AUTH_CB = (ENV === 'production') ? ('https://bee-companion.com/callback') : ('http://' + HOST + ':' + PORT + '/callback');
+const AUTH_CB = process.env.AUTH_CB = (ENV === 'production') ? ('https://bee-companion.com/#/callback') : ('http://' + HOST + ':' + PORT + '/#/callback');
 const HMR = helpers.hasProcessFlag('hot');
 const METADATA = webpackMerge(commonConfig({env: ENV}).metadata, {
   host: HOST,
