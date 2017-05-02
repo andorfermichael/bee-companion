28d122ee1a9b0d61ef004a0a50434d98182f5ecd
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2017-05-02 23:04:11 +0200

    [base] update database configuration

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -2,8 +2,11 @@ const dotenv = require('dotenv').config();
 
 module.exports = {
   development: {
-    database: "beecompanion-development",
-    host: "127.0.0.1",
+    username: process.env.DB_DEVELOPMENT_USERNAME,
+    password: process.env.DB_DEVELOPMENT_PASSWORD,
+    database: process.env.DB_DEVELOPMENT_NAME,
+    host: process.env.DB_DEVELOPMENT_HOST,
+    port: process.env.DB_DEVELOPMENT_PORT,
     dialect: "postgres"
   },
   test: {
