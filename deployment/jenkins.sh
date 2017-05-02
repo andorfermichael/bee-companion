95d106448b9727cc8eb174700551db94f8e3d85e
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2017-05-02 18:37:33 +0200

    [base] update deployment configuration

--- deployment/jenkins.sh
+++ deployment/jenkins.sh
@@ -1,7 +1,8 @@
-npm install
 npm install -g sequelize-cli
-sequelize --config ../config/dbconfig.js
+cd ..
+sequelize --config config/dbconfig.js
 sequelize db:migrate --env test
+npm install
 npm run api:prod:pm2
 npm run build:prod
 npm run server:prod:pm2
