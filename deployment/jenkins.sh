93954cb5e513b580b569c69f2868b76fa38351a2
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2017-05-02 19:05:30 +0200

    [base] update deployment configuration

--- deployment/jenkins.sh
+++ deployment/jenkins.sh
@@ -1,10 +1,7 @@
 cd /var/lib/jenkins/workspace/BeeCompanion/
-npm install dotenv
-npm install sequelize-cli
-npm install sequelize
+npm install
 sequelize --config config/dbconfig.js
 sequelize db:migrate --env test
-npm install
 npm run api:prod:pm2
 npm run build:prod
 npm run server:prod:pm2
