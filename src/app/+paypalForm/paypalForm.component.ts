85001ce70ec628b6fd16160e554e4e7d7a4acb25
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2017-05-08 17:11:00 +0200

    [feature] integrate paypal adaptive payments sdk

--- src/app/+paypalForm/paypalForm.component.ts
+++ src/app/+paypalForm/paypalForm.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { PayPalAdaptive } from '../providers/paypal.provider';
 
 @Component({
   selector: 'paypalForm',
   styleUrls: [ './paypalForm.component.scss' ],
   templateUrl: './paypalForm.component.html',
+  providers: [ PayPalAdaptive ]
 })
 export class PayPalFormComponent {
+  private paypalAdaptiveSDK: any;
 
+  constructor(private paypalAdaptive: PayPalAdaptive) {
+    this.paypalAdaptive = paypalAdaptive.getSDK();
+  }
+
+  public instantiatePayPalAdaptiveSDK(userId: string, password: string,
+                                      signature: string, sandbox: boolean) {
+    this.paypalAdaptiveSDK = new this.paypalAdaptiveSDK({
+      userId,
+      password,
+      signature,
+      sandbox // Defaults to false
+    });
+  }
 }
