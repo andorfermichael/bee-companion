3e5ceb7ed351061142aa7beae784e3d160501c28
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2017-05-23 15:43:54 +0200

    [tests] test cancelled payment

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,14 +1,14 @@
 import { Injectable, NO_ERRORS_SCHEMA} from '@angular/core';
-import { async, TestBed, ComponentFixture } from '@angular/core/testing';
+import {async, TestBed, ComponentFixture, fakeAsync, tick} from '@angular/core/testing';
 import { BaseRequestOptions, ConnectionBackend, Http } from '@angular/http';
 import { MockBackend } from '@angular/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { SpyLocation, MockLocationStrategy } from '@angular/common/testing';
+import { ActivatedRoute } from '@angular/router';
+import { Location, LocationStrategy } from '@angular/common';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 // Load the implementations that should be tested
-import { ActivatedRoute } from '@angular/router';
-import { Location, LocationStrategy } from '@angular/common';
 import { Auth } from '../@services/auth.service';
 import { PayPalService } from '../@services/paypal.service';
 import { LocalStorageService } from 'ngx-webstorage';
@@ -18,8 +18,10 @@ describe(`HomeComponent`, () => {
   let comp: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
   let authService: MockAuth;
+  let localStorageService: LocalStorageService;
   let activatedRoute: ActivatedRouteStub;
   let location: SpyLocation;
+  let paypalService: PayPalService;
 
   @Injectable()
   class ActivatedRouteStub {
@@ -44,8 +46,6 @@ describe(`HomeComponent`, () => {
 
   class MockAuth {
     public authenticated: boolean;
-
-    constructor() {}
 
     public isAuthenticated(): boolean {
       return this.authenticated;
@@ -87,28 +87,56 @@ describe(`HomeComponent`, () => {
     .compileComponents(); // compile template and css
   }));
 
-  // synchronous beforeEach
-  beforeEach(() => {
-    activatedRoute.testParams = { status: 'approved' };
-    location = TestBed.get(Location);
-    fixture = TestBed.createComponent(HomeComponent);
-    comp = fixture.componentInstance;
-    authService = TestBed.get(Auth);
+  describe(`initialization`, () => {
+    // synchronous beforeEach
+    beforeEach(() => {
+      location = TestBed.get(Location);
+      activatedRoute.testParams = { status: 'approved' };
+      fixture = TestBed.createComponent(HomeComponent);
+      comp = fixture.componentInstance;
+      authService = TestBed.get(Auth);
+    });
 
-    fixture.detectChanges(); // trigger initial data binding
+    it('ngOnInit should check user authentication state', () => {
+      spyOn(authService, 'isAuthenticated');
+      authService.authenticated = true;
+      fixture.detectChanges();
+      expect(authService.isAuthenticated).toHaveBeenCalled();
+    });
+
+    it('ngOnInit should check if authenticated user has role', () => {
+      spyOn(authService, 'checkUserHasRole');
+      authService.authenticated = true;
+      fixture.detectChanges();
+      expect(authService.checkUserHasRole).toHaveBeenCalled();
+    });
   });
 
-  it('ngOnInit should check user authentication state', () => {
-    spyOn(authService, 'isAuthenticated');
-    authService.authenticated = true;
-    comp.ngOnInit();
-    expect(authService.isAuthenticated).toHaveBeenCalled();
-  });
+  describe(`extended initialization when coming back from PayPal after payment cancel`, () => {
+    // synchronous beforeEach
+    beforeEach(() => {
+      location = TestBed.get(Location);
+      activatedRoute.testParams = { status: 'cancelled' };
+      fixture = TestBed.createComponent(HomeComponent);
+      comp = fixture.componentInstance;
+      authService = TestBed.get(Auth);
+      paypalService = TestBed.get(PayPalService);
+      localStorageService = TestBed.get(LocalStorageService);
+    });
 
-  it('ngOnInit should check if authenticated user has role', () => {
-    spyOn(authService, 'checkUserHasRole');
-    authService.authenticated = true;
-    comp.ngOnInit();
-    expect(authService.checkUserHasRole).toHaveBeenCalled();
+    it('ngOnInit should clear payKey from local storage', () => {
+      spyOn(localStorageService, 'clear');
+      authService.authenticated = true;
+      fixture.detectChanges();
+      expect(localStorageService.clear).toHaveBeenCalled();
+    });
+
+    it('ngOnInit should remove /payment/cancelled from url', fakeAsync(() => {
+      spyOn(location, 'replaceState');
+      authService.authenticated = true;
+      comp.ngOnInit();
+      tick(5000);
+      expect(location.replaceState).toHaveBeenCalled();
+    }));
   });
 });
