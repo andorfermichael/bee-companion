9a9cdb427d30e58ccc01ced42b5da84285998c80
Nico Deufemia <ndeufemia.mmt-b2014@fh-salzburg.ac.at>
2017-05-24 18:34:06 +0200

    [feature] prepare sidebar

---
+++ src/app/+sidenav/sidenav.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, ElementRef, HostListener, ViewChild, Input } from '@angular/core';
+import { trigger, state, style, transition, keyframes, animate } from '@angular/animations';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+
+import { Auth } from '../@services/auth.service';
+import { EventsService } from '../@services/events.service';
+
+@Component({
+  selector: 'side-nav',
+  styleUrls: ['./sidenav.component.scss'],
+  templateUrl: './sidenav.component.html'
+})
+export class SideNavComponent {
+  public static getOffsetLeft(element) {
+    return (element.offsetWidth / 2) + element.offsetLeft +
+      (element.offsetParent ? element.offsetParent.offsetLeft : 0);
+  }
+
+  constructor(private elemRef: ElementRef, private sanitizer: DomSanitizer,
+              public auth: Auth, private router: Router, public _eventsService: EventsService) {
+  }
+}
