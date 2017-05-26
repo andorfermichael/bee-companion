a549eff8bdfa651e7270e9cf9f28fd94b7947b9c
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2017-05-26 14:49:24 +0200

    [database] add migration for user

---
+++ api/models/user.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  const User = sequelize.define('User', {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    userName: DataTypes.STRING,
+    role: DataTypes.ENUM,
+    picture: DataTypes.STRING,
+    email: DataTypes.STRING,
+    paypalId: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    authenticated: DataTypes.BOOLEAN,
+    verified: DataTypes.BOOLEAN,
+    street: DataTypes.STRING,
+    streetNumber: DataTypes.STRING,
+    postalCode: DataTypes.STRING,
+    city: DataTypes.STRING,
+    country: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return User;
+};
