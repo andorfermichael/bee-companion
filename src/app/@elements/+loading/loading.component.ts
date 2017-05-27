be3e9ae96f6b44e1ec36fe8b42baf5562bc8b5f5
Nico Deufemia <ndeufemia.mmt-b2014@fh-salzburg.ac.at>
2017-05-27 20:11:22 +0200

    [feature] implement loading component

---
+++ src/app/@elements/+loading/loading.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'loading',
+  styleUrls: [ './loading.component.scss' ],
+  templateUrl: './loading.component.html'
+})
+
+export class LoadingComponent {
+}
